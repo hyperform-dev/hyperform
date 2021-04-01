1f933cbd0b0eea2d59b12a644b6ec292ee28a99f
qngapparat <jakob.wallnoefer@outlook.com>
2021-04-01 15:10:51 +0200

    google: don't bundle npm packages

--- bundler/google/index.js
+++ bundler/google/index.js
@@ -1,11 +1,26 @@
 const { _bundle } = require('../utils')
 /**
- * @description Bundles a given .js files for Google with its dependencies using webpack.
+ * @description Bundles a given .js files for Google using Webpack. IMPORTANT: excludes any npm packages. 
  * @param {string} inpath Path to entry .js file
  * @returns {Promise<string>} The bundled code
  */
 async function bundleGoogle(inpath) {
-  let externals // TODO check if @google is included on Google?
+  // Exclude any npm packages
+  // From https://github.com/webpack/webpack/issues/603#issuecomment-320191141
+  const externals = [
+    function (context, request, callback) {
+      // get absolute path to required file
+      const resourcePath = this.resolveSync(context, request);
+
+      // don't bundle node_modules
+      if (/node_modules/.test(resourcePath)) {
+        return callback(null, `commonjs ${request}`);
+      }
+      callback();
+    },
+  ] 
+  
+  // TODO check if @google is included on Google?
   const res = await _bundle(inpath, externals)
   return res
 }
