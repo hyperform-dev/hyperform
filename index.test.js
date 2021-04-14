72488b98b1bc153b30f9505ed26a7e09caf5ac86
qngapparat <jakob.wallnoefer@outlook.com>
2021-04-14 13:41:31 +0200

    update tests

--- index.test.js
+++ index.test.js
@@ -1,80 +1,70 @@
 /* eslint-disable no-await-in-loop, global-require */
 
 // One test to rule them all
-// (1) Deploy & publish a echo function via Hyperform to Amazon, Google
-//     -> The deployment, publishing worked correctly
-// (2) Ping it with an event
-//     -> The function completes
-// (3) Check whether echo is what we expect 
-//     -> The in-function wrapper worked correctly
 
 const os = require('os')
 const path = require('path')
 const fsp = require('fs').promises
-const fetch = require('node-fetch')
-const uuidv4 = require('uuid').v4
 
-const TIMEOUT = 2 * 60 * 1000
+const TIMEOUT = 1 * 60 * 1000
 
 describe('System tests (takes 1-2 minutes)', () => {
   describe('main', () => {
-    test('completes, and echo endpoints return first arg (event) and second arg (http) on GET and POST', async () => {
-      const { main } = require('./index')
-      /// ////////////////////////////////////////////
-      // Set up
+    describe('amazon', () => {
+      test('completes', async () => {
+        const { main } = require('./index')
+        /// ////////////////////////////////////////////
+        // Set up
 
-      const tmpdir = path.join(
-        os.tmpdir(),
-        `${Math.ceil(Math.random() * 100000000000)}`,
-      )
+        const tmpdir = path.join(
+          os.tmpdir(),
+          `${Math.ceil(Math.random() * 100000000000)}`,
+        )
 
-      // What we will pass to the functions
+        // // What we will pass to the functions
 
-      const random_string = uuidv4()
-      const event_body = {
-        random_string,
-      }
-      const event_querystring = `?random_string=${random_string}`
+        // const random_string = uuidv4()
+        // const event_body = {
+        //   random_string,
+        // }
+        // const event_querystring = `?random_string=${random_string}`
       
-      // Create javascript files
+        // Create javascript files
 
-      await fsp.mkdir(tmpdir)
-      const code = `
+        await fsp.mkdir(tmpdir)
+        const code = `
         function irrelevant() {
           return 100
         }
         
-        function jest_systemtest_echo(event, http) {
-          return { event: event, http: http }
+        function jest_systemtest_amazon(event, context, callback) {
+          context.succeed({})
         }
         
         module.exports = {
-          jest_systemtest_echo
+          jest_systemtest_amazon
         }
         `
 
-      const tmpcodepath = path.join(tmpdir, 'index.js')
-      await fsp.writeFile(tmpcodepath, code, { encoding: 'utf-8' })
+        const tmpcodepath = path.join(tmpdir, 'index.js')
+        await fsp.writeFile(tmpcodepath, code, { encoding: 'utf-8' })
     
-      const arg1 = tmpdir
-      const arg2 = tmpcodepath
-      /// ////////////////////////////////////////////
-      // Run main for Amazon
-      /// ////////////////////////////////////////////
-      let amazonMainRes
-
-      {
-        const amazonarg3 = {
+        const arg1 = tmpdir
+        const arg2 = tmpcodepath
+        /// ////////////////////////////////////////////
+        // Run main for Amazon
+        /// ////////////////////////////////////////////
+        let amazonMainRes
+        
+        const amazonarg3 = 'amazon'
+        const amazonarg4 = {
           amazon: {
             aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
             aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
-            aws_default_region: process.env.AWS_REGION,
+            aws_region: process.env.AWS_REGION,
           },
           
         }
-        
-        // isPublic
-        const amazonarg4 = true
         
         let err
         try {
@@ -86,28 +76,66 @@ describe('System tests (takes 1-2 minutes)', () => {
 
         // Expect main did not throw
         expect(err).not.toBeDefined()
-        // Expect main returned sensible data
-        expect(amazonMainRes).toBeDefined()
-        expect(amazonMainRes.urls).toBeDefined()
-      }
-        
-      /// ////////////////////////////////////////////
-      // Run main for Google
-      /// ////////////////////////////////////////////  
-      let googleMainRes
-      {
-        const googlearg3 = {
-          google: {
-            gc_client_email: '',
-            gc_private_key: '',
-            gc_project: '',
-          },
+        // // Expect main returned sensible data
+        // expect(amazonMainRes).toBeDefined()
+        // expect(amazonMainRes.urls).toBeDefined()
+      }, TIMEOUT)
+    })
 
+    describe('google', () => {
+      test('completes', async () => {
+        const { main } = require('./index')
+        /// ////////////////////////////////////////////
+        // Set up
+
+        const tmpdir = path.join(
+          os.tmpdir(),
+          `${Math.ceil(Math.random() * 100000000000)}`,
+        )
+
+        // // What we will pass to the functions
+
+        // const random_string = uuidv4()
+        // const event_body = {
+        //   random_string,
+        // }
+        // const event_querystring = `?random_string=${random_string}`
+      
+        // Create javascript files
+
+        await fsp.mkdir(tmpdir)
+        const code = `
+        function irrelevant() {
+          return 100
+        }
+        
+        function jest_systemtest_google(req, resp) {
+          resp.json({})
+        }
+        
+        module.exports = {
+          jest_systemtest_google
+        }
+        `
+
+        const tmpcodepath = path.join(tmpdir, 'index.js')
+        await fsp.writeFile(tmpcodepath, code, { encoding: 'utf-8' })
+    
+        const arg1 = tmpdir
+        const arg2 = tmpcodepath
+        /// ////////////////////////////////////////////
+        // Run main for Google
+        /// ////////////////////////////////////////////  
+        let googleMainRes
+        
+        const googlearg3 = 'google'
+        const googlearg4 = {
+          google: {
+            gc_project: process.env.GC_PROJECT,
+            gc_region: process.env.GC_REGION,
+          },
         }
 
-        // to test publishing too
-        const googlearg4 = true
-     
         let err
         try {
           googleMainRes = await main(arg1, arg2, googlearg3, googlearg4)
@@ -118,67 +146,172 @@ describe('System tests (takes 1-2 minutes)', () => {
 
         // Expect main did not throw
         expect(err).not.toBeDefined()
-        // Expect main returned sensible data
-        expect(googleMainRes).toBeDefined()
-        expect(googleMainRes.urls).toBeDefined()
-      }
+        // // Expect main returned sensible data
+        // expect(googleMainRes).toBeDefined()
+        // expect(googleMainRes.urls).toBeDefined()
+      }, TIMEOUT)
+    })
+    // test('completes, and echo endpoints return first arg (event) and second arg (http) on GET and POST', async () => {
+    //   const { main } = require('./index')
+    //   /// ////////////////////////////////////////////
+    //   // Set up
 
-      /// ///////////////////////////////////////////
-      // Ping each Amazon URL
-      // Expect correct result
-      /// ///////////////////////////////////////////
-      // Don't test Google ones, they take another 1-2min to be ready
-      const urls = [].concat(...amazonMainRes.urls)
-      // TODO ensure in deployGoogle we return only on truly completed 
-      // TODO then, we can start testing them here again
+    //   const tmpdir = path.join(
+    //     os.tmpdir(),
+    //     `${Math.ceil(Math.random() * 100000000000)}`,
+    //   )
+
+    //   // // What we will pass to the functions
+
+    //   // const random_string = uuidv4()
+    //   // const event_body = {
+    //   //   random_string,
+    //   // }
+    //   // const event_querystring = `?random_string=${random_string}`
+      
+    //   // Create javascript files
+
+    //   await fsp.mkdir(tmpdir)
+    //   const code = `
+    //     function irrelevant() {
+    //       return 100
+    //     }
         
-      for (let i = 0; i < urls.length; i += 1) {
-        const url = urls[i]
-        /// /////////////////
-        // POST /////////////
-        /// /////////////////
+    //     function jest_systemtest_echo(event, http) {
+    //       return { event: event, http: http }
+    //     }
+        
+    //     module.exports = {
+    //       jest_systemtest_echo
+    //     }
+    //     `
 
-        {
-          const postres = await fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(event_body),
-          })
-          const statusCode = postres.status
-          const actualResult = await postres.json()
-          // HTTP Code 2XX
-          expect(/^2/.test(statusCode)).toBe(true)
-          // Echoed event
-          expect(actualResult.event).toEqual(event_body)
-          // Returned second argument; check if the wrapper formed it properly
-          expect(actualResult.http).toBeDefined()
-          expect(actualResult.http.headers).toBeDefined()
-          expect(actualResult.http.method).toBe('POST')
-        }
+    //   const tmpcodepath = path.join(tmpdir, 'index.js')
+    //   await fsp.writeFile(tmpcodepath, code, { encoding: 'utf-8' })
+    
+    //   const arg1 = tmpdir
+    //   const arg2 = tmpcodepath
+    //   /// ////////////////////////////////////////////
+    //   // Run main for Amazon
+    //   /// ////////////////////////////////////////////
+    //   let amazonMainRes
 
-        /// /////////////////
-        // GET /////////////
-        /// /////////////////
+    //   {
+    //     const amazonarg3 = {
+    //       amazon: {
+    //         aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
+    //         aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
+    //         aws_region: process.env.AWS_REGION,
+    //       },
+          
+    //     }
+        
+    //     // isPublic
+    //     const amazonarg4 = true
+        
+    //     let err
+    //     try {
+    //       amazonMainRes = await main(arg1, arg2, amazonarg3, amazonarg4)
+    //     } catch (e) {
+    //       console.log(e)
+    //       err = e
+    //     }
 
-        {
-          const getres = await fetch(`${url}${event_querystring}`, {
-            method: 'GET',
-          })
-          const statusCode = getres.status
-          const actualResult = await getres.json()
-          // HTTP Code 2XX
-          expect(/^2/.test(statusCode)).toBe(true)
-          // Echoed event
-          expect(actualResult.event).toEqual(event_body)
-          // Returned second argument; check if the wrapper formed it properly
-          expect(actualResult.http).toBeDefined()
-          expect(actualResult.http.headers).toBeDefined()
-          expect(actualResult.http.method).toBe('GET')
-        }
-      }
-    }, TIMEOUT)
+    //     // Expect main did not throw
+    //     expect(err).not.toBeDefined()
+    //     // Expect main returned sensible data
+    //     expect(amazonMainRes).toBeDefined()
+    //     expect(amazonMainRes.urls).toBeDefined()
+    //   }
+        
+    //   /// ////////////////////////////////////////////
+    //   // Run main for Google
+    //   /// ////////////////////////////////////////////  
+    //   let googleMainRes
+    //   {
+    //     const googlearg3 = {
+    //       google: {
+    //         gc_client_email: '',
+    //         gc_private_key: '',
+    //         gc_project: '',
+    //       },
+
+    //     }
+
+    //     // to test publishing too
+    //     const googlearg4 = true
+     
+    //     let err
+    //     try {
+    //       googleMainRes = await main(arg1, arg2, googlearg3, googlearg4)
+    //     } catch (e) {
+    //       console.log(e)
+    //       err = e
+    //     }
+
+    //     // Expect main did not throw
+    //     expect(err).not.toBeDefined()
+    //     // Expect main returned sensible data
+    //     expect(googleMainRes).toBeDefined()
+    //     expect(googleMainRes.urls).toBeDefined()
+    //   }
+
+    //   /// ///////////////////////////////////////////
+    //   // Ping each Amazon URL
+    //   // Expect correct result
+    //   /// ///////////////////////////////////////////
+    //   // Don't test Google ones, they take another 1-2min to be ready
+    //   const urls = [].concat(...amazonMainRes.urls)
+    //   // TODO ensure in deployGoogle we return only on truly completed 
+    //   // TODO then, we can start testing them here again
+        
+    //   for (let i = 0; i < urls.length; i += 1) {
+    //     const url = urls[i]
+    //     /// /////////////////
+    //     // POST /////////////
+    //     /// /////////////////
+
+    //     {
+    //       const postres = await fetch(url, {
+    //         method: 'POST',
+    //         body: JSON.stringify(event_body),
+    //       })
+    //       const statusCode = postres.status
+    //       const actualResult = await postres.json()
+    //       // HTTP Code 2XX
+    //       expect(/^2/.test(statusCode)).toBe(true)
+    //       // Echoed event
+    //       expect(actualResult.event).toEqual(event_body)
+    //       // Returned second argument; check if the wrapper formed it properly
+    //       expect(actualResult.http).toBeDefined()
+    //       expect(actualResult.http.headers).toBeDefined()
+    //       expect(actualResult.http.method).toBe('POST')
+    //     }
+
+    //     /// /////////////////
+    //     // GET /////////////
+    //     /// /////////////////
+
+    //     {
+    //       const getres = await fetch(`${url}${event_querystring}`, {
+    //         method: 'GET',
+    //       })
+    //       const statusCode = getres.status
+    //       const actualResult = await getres.json()
+    //       // HTTP Code 2XX
+    //       expect(/^2/.test(statusCode)).toBe(true)
+    //       // Echoed event
+    //       expect(actualResult.event).toEqual(event_body)
+    //       // Returned second argument; check if the wrapper formed it properly
+    //       expect(actualResult.http).toBeDefined()
+    //       expect(actualResult.http.headers).toBeDefined()
+    //       expect(actualResult.http.method).toBe('GET')
+    //     }
+    //   }
+    // }, TIMEOUT)
   })
 
-  describe('cli', () => {
-    // TODO
-  })
+  // describe('cli', () => {
+  //   // TODO
+  // })
 })
--- schemas/index.test.js
+++ schemas/index.test.js
@@ -1,100 +1,96 @@
 /* eslint-disable global-require */
 describe('schemas', () => {
-  describe('hyperform.json schema', () => {
-    describe('valid cases', () => {
-      test('allows empty string fields in amazon', () => {
-        const { hyperformJsonSchema } = require('./index')
+  describe('amazon schema', () => {
+    test('normal case', () => {
+      const { amazonSchema } = require('./index')
 
-        const input = {
-          amazon: {
-            aws_access_key_id: '',
-            aws_secret_access_key: '',
-            aws_default_region: '',
-          },
-        }
+      const input = {
+        amazon: {
+          aws_access_key_id: 'xx',
+          aws_secret_access_key: 'xx',
+          aws_region: 'xx',
+        },
+      }
 
-        const { error } = hyperformJsonSchema.validate(input)
-        expect(error).not.toBeDefined()
-      })
-
-      test('allows aws_session_token field', () => {
-        const { hyperformJsonSchema } = require('./index')
-
-        const input = {
-          amazon: {
-            aws_access_key_id: '',
-            aws_secret_access_key: '',
-            aws_default_region: '',
-            aws_session_token: '',
-          },
-        }
-
-        const { error } = hyperformJsonSchema.validate(input)
-        expect(error).not.toBeDefined()
-      })
-
-      test('allows normal string fields in google', () => {
-        const { hyperformJsonSchema } = require('./index')
-
-        const input = {
-          google: {
-            gc_project: 'abc',
-            gc_client_email: 'abc',
-            gc_private_key: 'abc',
-          },
-        }
-
-        const { error } = hyperformJsonSchema.validate(input)
-        expect(error).not.toBeDefined()
-      })
+      const { error } = amazonSchema.validate(input)
+      expect(error).not.toBeDefined()
     })
 
-    describe('invalid cases', () => {
-      test('does not allow both providers', () => {
-        const { hyperformJsonSchema } = require('./index')
+    test('allows aws_session_token field', () => {
+      const { amazonSchema } = require('./index')
 
-        const input = {
-          amazon: {
-            aws_access_key_id: 'abc',
-            aws_secret_access_key: 'abc',
-            aws_default_region: 'abc',
-          },
-          google: {
-            gc_project: 'abc',
-            gc_client_email: 'abc',
-            gc_private_key: 'abc',
-          },
-        }
+      const input = {
+        amazon: {
+          aws_access_key_id: 'xx',
+          aws_secret_access_key: 'xx',
+          aws_region: 'xx',
+          aws_session_token: 'xx',
+        },
+      }
 
-        const { error } = hyperformJsonSchema.validate(input)
-        expect(error).toBeDefined()
-      })
+      const { error } = amazonSchema.validate(input)
+      expect(error).not.toBeDefined()
+    })
 
-      test('does not allow no provider', () => {
-        const { hyperformJsonSchema } = require('./index')
+    test('does not allow missing field in amazon', () => {
+      const { amazonSchema } = require('./index')
 
-        const input = {
-          // empty
-        }
+      const input = {
+        amazon: {
+          aws_access_key_id: 'xx',
+          aws_secret_access_key: 'xx',
+          // Missing: aws_region: '',
+        },
+      }
 
-        const { error } = hyperformJsonSchema.validate(input)
-        expect(error).toBeDefined()
-      })
+      const { error } = amazonSchema.validate(input)
+      expect(error).toBeDefined()
+    })
 
-      test('does not allow missing field in amazon', () => {
-        const { hyperformJsonSchema } = require('./index')
+    test('does not allow both providers', () => {
+      const { amazonSchema } = require('./index')
 
-        const input = {
-          amazon: {
-            aws_access_key_id: '',
-            aws_secret_access_key: '',
-            // Missing: aws_default_region: '',
-          },
-        }
+      const input = {
+        amazon: {
+          aws_access_key_id: 'abc',
+          aws_secret_access_key: 'abc',
+          aws_region: 'abc',
+        },
+        google: {
+          gc_project: 'abc',
+          gc_region: 'abc',
+        },
+      }
 
-        const { error } = hyperformJsonSchema.validate(input)
-        expect(error).toBeDefined()
-      })
+      const { error } = amazonSchema.validate(input)
+      expect(error).toBeDefined()
+    })
+
+    test('does not allow no provider', () => {
+      const { amazonSchema } = require('./index')
+
+      const input = {
+        // empty
+      }
+
+      const { error } = amazonSchema.validate(input)
+      expect(error).toBeDefined()
+    })
+  })
+
+  describe('google schema', () => {
+    test('normal case', () => {
+      const { googleSchema } = require('./index')
+
+      const input = {
+        google: {
+          gc_project: 'abc',
+          gc_region: 'abc',
+        },
+      }
+
+      const { error } = googleSchema.validate(input)
+      expect(error).not.toBeDefined()
     })
   })
 })
